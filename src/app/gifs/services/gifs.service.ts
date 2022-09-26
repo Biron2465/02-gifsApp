3ecfd4939b4cda3b927233f186af1ec5086067fe
xllAlonsollx <biron2465@gmail.com>
2022-09-25 20:37:01 -0600

    Update gifs.service.ts

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { SearchGifsResponse, Gif } from '../interface/gifs.interface';
-import { jsDocComment } from '@angular/compiler';
+
 
 @Injectable({
   providedIn: 'root'
@@ -49,6 +49,6 @@ export class GifsService {
       .subscribe((resp) =>{
         this.resultados= resp.data;
         localStorage.setItem('resultados', JSON.stringify(this.resultados));
-      })
+    })
   }
 }
